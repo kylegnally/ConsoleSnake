3b6e684714089632336344aaaafe39c11f2948d4
Kyle G. Nally <kylegnally@hotmail.com>
2020-10-20 13:37:01 -0400

    Initial commit. Write Coord class.

--- Coord.cs
+++ Coord.cs
@@ -1,12 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Permissions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ConsoleSnake
 {
-    class Coord
+    public class Coord
     {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Coord()
+        {
+            X = 0;
+            Y = 0;
+        }
+
+        public Coord(Coord input)
+        {
+            X = input.X;
+            Y = input.Y;
+        }
+
+        public Coord(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
     }
 }
