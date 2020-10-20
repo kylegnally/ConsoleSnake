5eb7a2de34b72b4f848429336fbde6f34d7c6f37
Kyle G. Nally <kylegnally@hotmail.com>
2020-10-20 13:49:21 -0400

    Write Direction class.

--- Direction.cs
+++ Direction.cs
@@ -8,5 +8,21 @@ namespace ConsoleSnake
 {
     class Direction : Snake
     {
+        public enum Dir
+        {
+            Up,
+            Right,
+            Down,
+            Left
+        }
+
+        public Dir NewDirection { get; set; }
+        public Dir OldDirection { get; set; }
+
+        public Direction()
+        {
+            NewDirection = Dir.Down;
+            OldDirection = NewDirection;
+        }
     }
 }
